01e3d058313c9a84625355d6ae0576f72757df90
Prajwal <prajwalr19@gmail.com>
2022-06-30 00:08:14 +0530

    kill

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,19 +1,6 @@
 #!/bin/sh -l
 
-echo "::debug ::Debug Message"
-echo "::warning ::Warning Message"
-echo "::error ::Error Message"
-
-echo"::add-mask::$1"
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-
-echo "::group::Some expandable logs"
-echo 'anything'
-echo 'anything'
-echo 'anything'
-echo '::end group::'
-
-echo '::set-env name=HELLO::hello'
 
